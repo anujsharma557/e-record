6f082622c12302fb78360f10bd97a06e46e0c702
anujsharma557 <sharma.anuj557@gmail.com>
2021-01-31 01:18:55 +0530

    EC-1| updated class

--- e-catalogue/src/main/java/com/et/controllers/eComController.java
+++ e-catalogue/src/main/java/com/et/controllers/eComController.java
@@ -1,12 +1,38 @@
 package com.et.controllers;
 
+import com.et.model.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author: Anuj Sharma
+ */
 @RestController
 public class eComController {
+    /**
+     * method returns product Id
+     * @return
+     */
     @RequestMapping("/addProduct")
     public long addProduct(){
         return 100L;
+    }
+
+    /**
+     * method returns product Id
+     * @return
+     */
+    @RequestMapping("/updateProduct")
+    public long updateProduct(){
+        return 100L;
+    }
+
+    /**
+     * method returns product Details
+     * @return
+     */
+    @RequestMapping("/getProduct")
+    public Product getProduct(){
+        return null;
     }
 }
